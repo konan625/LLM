01e47aacf85ae29748462d4512622fcaec3aff7d
aanchal625 <82753429+aanchal625@users.noreply.github.com>
2025-11-03 02:04:47 +0530

    using local weights models through ollama

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
 
 def main():
     print("Hello from llm!")
-    print(os.getenv("OPENAI_API_KEY"))
+    # print(os.getenv("OPENAI_API_KEY"))
+    information = """
+    Donald John Trump (born June 14, 1946) is an American politician, media personality, and businessman who is the 47th president of the United States. A member of the Republican Party, he served as the 45th president from 2017 to 2021.
+
+    Born into a wealthy family in New York City, Trump graduated from the University of Pennsylvania in 1968 with a bachelor's degree in economics. He became the president of his father's real estate business in 1971, renamed it The Trump Organization, and began acquiring and building skyscrapers, hotels, casinos, and golf courses. He also launched side ventures, many licensing the Trump name, and filed for six business bankruptcies in the 1990s and 2000s. From 2004 to 2015, he hosted the reality television show The Apprentice, bolstering his fame as a billionaire. Presenting himself as a political outsider, Trump won the 2016 presidential election against Democratic Party nominee Hillary Clinton.
+
+    During his first presidency, Trump imposed a travel ban on seven Muslim-majority countries, expanded the Mexico–United States border wall, and enforced a family separation policy on the border. He rolled back environmental and business regulations, signed the Tax Cuts and Jobs Act, and appointed three Supreme Court justices. In foreign policy, Trump withdrew the U.S. from agreements on climate, trade, and Iran's nuclear program, and initiated a trade war with China. In response to the COVID-19 pandemic from 2020, he downplayed its severity, contradicted health officials, and signed the CARES Act. After losing the 2020 presidential election to Joe Biden, Trump attempted to overturn the result, culminating in the January 6 Capitol attack in 2021. He was impeached in 2019 for abuse of power and obstruction of Congress, and in 2021 for incitement of insurrection; the Senate acquitted him both times.
+
+    In 2023, Trump was found liable in civil cases for sexual abuse and defamation and for business fraud. He was found guilty of falsifying business records in 2024, making him the first U.S. president convicted of a felony. After winning the 2024 presidential election against Kamala Harris, he was sentenced to a penalty-free discharge, and two felony indictments against him for retention of classified documents and obstruction of the 2020 election were dismissed without prejudice. A racketeering case related to the 2020 election in Georgia is pending.
+
+    Trump began his second presidency by initiating mass layoffs of federal workers. He imposed tariffs on nearly all countries at the highest level since the Great Depression and signed the One Big Beautiful Bill Act. His administration's actions—including intimidation of political opponents and civil society, reversal of pro-diversity policies, targeting of transgender people, deportations of immigrants, extensive use of executive orders, and his broad interpretation of the unitary executive theory—have drawn over 300 lawsuits challenging the legality and constitutionality of the actions.
+
+    Since 2015, Trump's leadership style and political agenda—often referred to as Trumpism—have reshaped the Republican Party's identity. Many of his comments and actions have been characterized as racist or misogynistic, and he has made false or misleading statements and promoted conspiracy theories to a degree unprecedented in American politics. Trump's actions, especially in his second term, have been described as authoritarian and contributing to democratic backsliding. After his first term, scholars and historians ranked him as one of the worst presidents in American history.
+    """
+    summary_template = """
+    Given the {information} I want you to tell me some very interesting points about the person mentioned there.
+    """
+
+    summary_promt = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    ) # we pass the placeholder value for the template
+
+    # llm = ChatOpenAI(temperature=0, model="gpt-5")
+    llm=ChatOllama(temperature=0,model="gemma3:270m") #this is a locally saved model through ollama
+    chain = (
+        summary_promt | llm
+    )  # this is langchain expression language lecl, we make chain like this first will be promt template the pipe operator then llm
+    response = chain.invoke(input={"information": information})
+    print(response.content)
 
 
 if __name__ == "__main__":
